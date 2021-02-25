be280c71e05193b4e18ab9ca9089547170603e34
Alomgir <bdalomgir@gmail.com>
2021-02-25 18:12:33 +0600

    Remove default Backdrop breadcrump separator

--- template.php
+++ template.php
@@ -15,3 +15,24 @@ function opera_preprocess_layout(&$variables) {
     $variables['theme_hook_suggestion'] = $original . '__front';
   }
 }
+
+/**
+ * Returns HTML for a breadcrumb trail.
+ *
+ * @param $variables
+ *   An associative array containing:
+ *   - breadcrumb: An array containing the breadcrumb links.
+ */
+function opera_breadcrumb($variables) {
+  $breadcrumb = $variables['breadcrumb'];
+  $output = '';
+  if (!empty($breadcrumb)) {
+    $output .= '<nav role="navigation" class="breadcrumb">';
+    // Provide a navigational heading to give context for breadcrumb links to
+    // screen-reader users. Make the heading invisible with .element-invisible.
+    $output .= '<h2 class="element-invisible">' . t('You are here') . '</h2>';
+    $output .= '<ol><li>' . implode('</li><li>', $breadcrumb) . '</li></ol>';
+    $output .= '</nav>';
+  }
+  return $output;
+}
